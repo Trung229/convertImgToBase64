1717255893882d0bc434acba876ba7c9fc1c902d
Trung <ductrung4263@gmail.com>
2021-12-06 14:41:55 +0700

    fix url

--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,9 @@ router.post("/upload/thumbnailManga",[upload.single('thumbnail')],async function
   let thumbnail;
   if(file){
     if(body.MangaId){
-      thumbnail = 'https://handle-img.travel4t.bandn.online/images/'+ body.MangaId+ "/" + body.count + "/" + file.filename;
+      thumbnail = 'https://handleimg.travel4t.bandn.online/images/'+ body.MangaId+ "/" + body.count + "/" + file.filename;
     }else{
-      thumbnail = 'https://handle-img.travel4t.bandn.online/images/thumbnails' + "/" + file.filename;
+      thumbnail = 'https://handleimg.travel4t.bandn.online/images/thumbnails' + "/" + file.filename;
 
     }
   }
